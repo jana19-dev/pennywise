31909e5c358e34b4c5d4b5400bce204e405153ff
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2023-03-21 21:23:30 -0400

    Only show account types with active accounts in sidebar

--- src/lib/components/sidebar/Sidebar.svelte
+++ src/lib/components/sidebar/Sidebar.svelte
@@ -60,21 +60,23 @@
       {:else}
         <div class="flex flex-1 flex-col gap-2 overflow-y-auto px-1">
           {#each $accountTypesQueryResult.data as accountType (accountType.id)}
-            <span
-              class="inline-flex w-fit items-center rounded bg-purple-300 px-2 py-0.5 text-xs font-medium text-purple-900"
-              >{accountType.name}</span
-            >
-            {#each accountType.accounts as account (account.id)}
-              <SidebarLink
-                link={{
-                  label: account.name,
-                  href: `/transactions/${account.id}`,
-                  iconPaths: [
-                    `M1 4a1 1 0 011-1h16a1 1 0 011 1v8a1 1 0 01-1 1H2a1 1 0 01-1-1V4zm12 4a3 3 0 11-6 0 3 3 0 016 0zM4 9a1 1 0 100-2 1 1 0 000 2zm13-1a1 1 0 11-2 0 1 1 0 012 0zM1.75 14.5a.75.75 0 000 1.5c4.417 0 8.693.603 12.749 1.73 1.111.309 2.251-.512 2.251-1.696v-.784a.75.75 0 00-1.5 0v.784a.272.272 0 01-.35.25A49.043 49.043 0 001.75 14.5z`
-                  ]
-                }}
-              />
-            {/each}
+            {#if accountType.accounts.length > 0}
+              <span
+                class="inline-flex w-fit items-center rounded bg-purple-300 px-2 py-0.5 text-xs font-medium text-purple-900"
+                >{accountType.name}</span
+              >
+              {#each accountType.accounts as account (account.id)}
+                <SidebarLink
+                  link={{
+                    label: account.name,
+                    href: `/transactions/${account.id}`,
+                    iconPaths: [
+                      `M1 4a1 1 0 011-1h16a1 1 0 011 1v8a1 1 0 01-1 1H2a1 1 0 01-1-1V4zm12 4a3 3 0 11-6 0 3 3 0 016 0zM4 9a1 1 0 100-2 1 1 0 000 2zm13-1a1 1 0 11-2 0 1 1 0 012 0zM1.75 14.5a.75.75 0 000 1.5c4.417 0 8.693.603 12.749 1.73 1.111.309 2.251-.512 2.251-1.696v-.784a.75.75 0 00-1.5 0v.784a.272.272 0 01-.35.25A49.043 49.043 0 001.75 14.5z`
+                    ]
+                  }}
+                />
+              {/each}
+            {/if}
           {/each}
         </div>
       {/if}
--- src/lib/components/table/TableHeaderCell.svelte
+++ src/lib/components/table/TableHeaderCell.svelte
@@ -5,6 +5,6 @@
 <th
   scope="col"
   {colspan}
-  class="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-80 py-3.5 pl-4 pr-3 text-left text-xs font-semibold text-gray-900 backdrop-blur backdrop-filter"
+  class="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-80 py-5 pl-4 pr-3 text-left text-xs font-semibold text-gray-900 backdrop-blur backdrop-filter"
   ><slot /></th
 >
