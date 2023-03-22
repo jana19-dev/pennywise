e74179f1443592c776d067e8ad5b7f99c2da7bc2
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2023-03-22 00:09:23 -0400

    Use global prisma client for dev environment

--- src/lib/utils/server/prisma.js
+++ src/lib/utils/server/prisma.js
@@ -1,5 +1,7 @@
 import { PrismaClient } from "@prisma/client"
 
-const prisma = new PrismaClient()
+const globalForPrisma = global
 
-export default prisma
+export const prisma = globalForPrisma.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV !== `production`) globalForPrisma.prisma = prisma
